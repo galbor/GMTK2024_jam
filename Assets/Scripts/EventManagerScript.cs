89e8a45a880bf978df28dea115aa506ae3005da9
galborovoi <gal.borovoi@huji.mail.ac.il>
2024-08-16 19:06:23 +0300

    first commit

---
+++ Assets/Scripts/EventManagerScript.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine.Events;
+using System.Collections.Generic;
+using Effects;
+
+public class EventManagerScript : Singleton<EventManagerScript>
+{
+	public const string StartGame = "StartGame";
+    public const string PlayerFirstLand = "PlayerFirstLand";
+	public const string WaterHit = "WaterHit";
+	public const string PlayerDrowned = "PlayerDrowned";
+	public const string PlayerHit = "PlayerHit";
+	public const string Win = "Win";
+    public const string Lose = "Lose";
+	public const string StartDescent = "start descent";
+	public const string StopDescent = "stop descent";
+    public const string DishWithDishCollision = "DishWithDishCollision";
+
+    private bool restarted = false; //used to check if the game was restarted
+									//I pur it here because it's a singleton with DontDestroyOnLoad
+	
+
+	protected EventManagerScript()
+    {
+        Init();
+    } // guarantee this will be always a singleton only - can't use the constructor!
+
+    public class FloatEvent : UnityEvent<object> {} //empty class; just needs to exist
+
+
+    private Dictionary <string, FloatEvent> eventDictionary;
+
+    private void Init ()
+	{
+		if (eventDictionary == null)
+		{
+			eventDictionary = new Dictionary<string, FloatEvent>();
+		}
+	}
+	
+	public void StartListening (string eventName, UnityAction<object> listener)
+	{
+		FloatEvent thisEvent = null;
+		if (eventDictionary.TryGetValue (eventName, out thisEvent))
+		{
+			thisEvent.AddListener (listener);
+		} 
+		else
+		{
+			thisEvent = new FloatEvent ();
+			thisEvent.AddListener (listener);
+			eventDictionary.Add (eventName, thisEvent);
+		}
+	}
+	
+	public void StopListening (string eventName, UnityAction<object> listener)
+	{
+		FloatEvent thisEvent = null;
+		if (eventDictionary.TryGetValue (eventName, out thisEvent))
+		{
+			thisEvent.RemoveListener (listener);
+		}
+	}
+	
+	public void TriggerEvent (string eventName, object obj)
+	{
+		FloatEvent thisEvent = null;
+		if (eventDictionary.TryGetValue (eventName, out thisEvent))
+		{
+			thisEvent.Invoke (obj);
+		}
+	}
+	
+	public void restart()
+	{
+		ScreenEffects.StopShake();
+		restarted = true;
+	}
+	
+	public bool isRestarted()
+	{
+		return restarted;
+	}
+}
